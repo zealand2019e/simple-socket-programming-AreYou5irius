6144a8ff31166a61bcc47ba3e12d426ad7716318
Maja Seehusen Bloch <maja8202@edu.easj.dk>
2020-09-06 17:29:57 +0200

    now with an Echo client, and changes to the server to count words

---
+++ EchoClient/Client.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace EchoClient
+{
+    class Client
+    {
+        public static void Start()
+        {
+            TcpClient socket = null;
+
+            Int32 port = 7777;
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+            socket = new TcpClient(localAddr.ToString(), port);
+
+            // Get a stream object for reading and writing
+            NetworkStream stream = socket.GetStream();
+
+            StreamReader sr = new StreamReader(stream);
+            StreamWriter sw = new StreamWriter(stream);
+
+
+            sw.WriteLine("Hej");
+            sw.Flush();
+
+            string line = sr.ReadLine();
+
+            Console.WriteLine(line);
+
+        }
+    }
+}
--- EchoServer/Server.cs
+++ EchoServer/Server.cs
@@ -9,7 +9,7 @@ namespace EchoServer
 {
     class Server
     {
-       public static void Start()
+        public static void Start()
         {
 
 
@@ -17,50 +17,83 @@ namespace EchoServer
 
 
             //Set the TcpListener on port 7777. 
-                Int32 port = 7777;
-                IPAddress localAddr = IPAddress.Loopback;
+            Int32 port = 7777;
+            IPAddress localAddr = IPAddress.Loopback;
 
-                // TcpListener server = new TcpListener(port);
-                server = new TcpListener(localAddr, port);
+            // TcpListener server = new TcpListener(port);
+            server = new TcpListener(port);
 
-                // Start listening for client requests.
-                server.Start();
+            // Start listening for client requests.
+            server.Start();
 
-                //accept tcp client
+            //accept tcp client
 
-                TcpClient client = server.AcceptTcpClient();
-                Console.WriteLine("Server Activated");
+            TcpClient client = server.AcceptTcpClient();
+            Console.WriteLine("Server Activated");
+
+            //her benytter vi os af en client
+            DoClient(client);
+
+            //her stopper vi serveren
+            server.Stop();
+            Console.WriteLine("server stopped");
+
+        }
 
 
-                // Get a stream object for reading and writing
-                NetworkStream stream = client.GetStream();
+        public static void DoClient(TcpClient client)
+        {
+            // Get a stream object for reading and writing
+            NetworkStream stream = client.GetStream();
 
-                StreamReader sr = new StreamReader(stream);
-                StreamWriter sw = new StreamWriter(stream);
-                sw.AutoFlush = true; // enable automatic flushing
+            StreamReader sr = new StreamReader(stream);
+            StreamWriter sw = new StreamWriter(stream);
+            sw.AutoFlush = true; // enable automatic flushing
 
+
+            //her siger vi hvis vi skriver "luk" så lukker forbindelsen og ellers ændre den bare beskeden til store bogstaver
+            //while (true)
+            //{
+            //    string message = sr.ReadLine();
+
+            //    Console.WriteLine("received message: " + message);
+
+            //    if (message == "luk")
+            //    {
+            //        // Console.WriteLine("received message: " + message);
+            //        break;
+            //    }
+            //    else if (message != null)
+            //    {
+            //        sw.WriteLine(message.ToUpper());
+            //    }
+
+            //}
+
+
+            // her aflæses beskeden og bliver tildelt variable navnet message. herfra tager vi udgangspunkt i ordets længde.
+            //  ******** SKRIV FORKLARING
+            while (true)
+            {
                 string message = sr.ReadLine();
+                Console.WriteLine("receved message: " + message);
 
-                while (true)
-                {
+                var wordCount = 0;
+                for (var i = 0; i < message.Length; i++)
+                    if (message[i] == ' ' || i == message.Length - 1)
+                        wordCount++;
+                Console.WriteLine("number of words in massage :" + wordCount);
 
-                Console.WriteLine("received message: " + message);
-                    if (message != null)
-                    {
-                        sw.WriteLine(message.ToUpper());
-
-                    }
-                }
-
-                stream.Close();
-                Console.WriteLine("net stream closed");
-                client.Close();
-                Console.WriteLine("connection to client closed");
-                server.Stop();
-                Console.WriteLine("server stopped");
-
-                
             }
+
+
+            //her lukker vi forbindelsen og client
+            stream.Close();
+            Console.WriteLine("net stream closed");
+            client.Close();
+            Console.WriteLine("connection to client closed");
+        }
+
 
     }
 }
